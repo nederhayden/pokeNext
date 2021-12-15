20ffa344f3420db9f4a82adcccc3a97d84cc6438
Neder <nedersimoes@gmail.com>
2021-12-15 13:57:23 -0400

    create components

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
+import Navbar from "../components/Navbar";
+
 import styles from "../styles/Home.module.css";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <ul>
-        <li>
-          <Link href="/products">
-            <a>Produtos</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>Sobre</a>
-          </Link>
-        </li>
-      </ul>
+      <Navbar />
       <h1>Hello World!</h1>
     </div>
   );
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Navbar from "../../components/Navbar";
 
 export default function Products() {
   return (
     <>
+      <Navbar />
       <h1>Products</h1>
       <Link href="/">
         <a>Voltar</a>
