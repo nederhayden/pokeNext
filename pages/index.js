46f7218db738b5b9ba43ebeb1a0aaeef3ee66842
Neder <nedersimoes@gmail.com>
2021-12-16 11:38:24 -0400

    create component layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Navbar from "../components/Navbar";
-
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar />
       <h1>Hello World!</h1>
     </div>
   );
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navbar from "../../components/Navbar";
 
 export default function Products() {
   return (
     <>
-      <Navbar />
       <h1>Products</h1>
       <Link href="/">
         <a>Voltar</a>
