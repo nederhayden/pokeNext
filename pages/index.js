28f17db413c7f8eac5562cada75673bdf782a29a
Neder <nedersimoes@gmail.com>
2021-12-17 09:11:51 -0400

    add css styles

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hello World!</h1>
+    <div>
+      <h1 className={styles.title}>Hello World!</h1>
     </div>
   );
 }
